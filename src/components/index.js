efaa1738547904e7cf06d8b6f95c0c63c7fa65e3
sixertoy <sixertoy.github@gmail.com>
2018-07-29 18:30:34 +0200

    🐛 fix issues with folder name
    - src/components/form renamed to src/components/forms

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,9 @@
-export * from './form'
-export * from './hocs'
-
 import Icon from './Icon'
 import InfiniteScroller from './InfiniteScroller'
 import Loading from './Loading'
 import Modal from './Modal'
+
+export * from './forms'
+export * from './hocs'
 
 export { Icon, InfiniteScroller, Loading, Modal }
