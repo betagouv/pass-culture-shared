a500dbc09ad115bc06c159025ae47e978c6b9be0
sixertoy <sixertoy.github@gmail.com>
2018-07-29 18:34:24 +0200

    🐛 fix issue with property named props.name in Icon.js

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -3,14 +3,13 @@ import React from 'react'
 
 import { ROOT_PATH } from '../utils/config'
 
-const Icon = ({ svg, ...imgProps }) => {
+const Icon = ({ svg, name, ...imgProps }) => {
   if (svg) {
     return <img src={`${ROOT_PATH}/icons/${svg}.svg`} alt={svg} {...imgProps} />
-  } else {
-    const iconName =
-      'Md' + props.name.replace(/(^|-)(\w)/g, (m0, m1, m2) => m2.toUpperCase())
-    return reactIconPack[iconName]()
   }
+  const iconName =
+    'Md' + name.replace(/(^|-)(\w)/g, (m0, m1, m2) => m2.toUpperCase())
+  return reactIconPack[iconName]()
 }
 
 export default Icon
