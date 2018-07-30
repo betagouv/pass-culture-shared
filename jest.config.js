377861fb4cf66213199c77195278493b9bd9cd9c
Anne-Marie Esteves <amesteves@octo.com>
2018-07-30 15:21:35 +0200

    add first test and jest minimum configuration

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  verbose: true,
+  testEnvironment: 'node',
+  'testURL': 'http://localhost',
+  transformIgnorePatterns: ['<rootDir>/node_modules/', '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$'],
+  "transform": {
+  "^.+\\.js$": "babel-jest",
+  "^.+\\.js$": "babel-jest"
+  }
+}
