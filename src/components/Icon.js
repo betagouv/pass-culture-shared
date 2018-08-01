b6b2f69583e55422c0f57652f603dd0c374061d2
erwanledoux <lawanledoux@gmail.com>
2018-08-02 01:19:42 +0200

    adapted Icon with rootPath

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,11 +1,12 @@
 import * as reactIconPack from 'react-icons/lib/md'
 import React from 'react'
 
-import { ROOT_PATH } from '../utils/config'
-
 const Icon = ({ svg, name, ...imgProps }) => {
+  if (!Icon.rootPath) {
+    console.warn('You need to define a rootPath for your Icon')
+  }
   if (svg) {
-    return <img src={`${ROOT_PATH}/icons/${svg}.svg`} alt={svg} {...imgProps} />
+    return <img src={`${Icon.rootPath}/icons/${svg}.svg`} alt={svg} {...imgProps} />
   }
   const iconName =
     'Md' + name.replace(/(^|-)(\w)/g, (m0, m1, m2) => m2.toUpperCase())
