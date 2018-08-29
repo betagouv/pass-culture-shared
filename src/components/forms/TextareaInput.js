999e890f0a40673fb68804cc2c7d2f51d27ec615
erwanledoux <lawanledoux@gmail.com>
2018-08-29 11:28:30 +0200

    added maxLength display

--- src/components/forms/TextareaInput.js
+++ src/components/forms/TextareaInput.js
@@ -1,25 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Textarea from 'react-autosize-textarea'
 
-const TextareaInput = props => {
-  const { onChange: fieldOnChange } = props
+class TextareaInput extends Component {
 
-  const onChange = event => fieldOnChange(event.target.value, { event })
+  onChange = event => {
+    const {
+      maxLength,
+      onChange: fieldOnChange,
+    } = this.props
 
-  return (
-    <Textarea
-      aria-describedby={props['aria-describedby']}
-      autoComplete={props.autoComplete}
-      className={`textarea is-${props.size}`}
-      id={props.id}
-      name={props.name}
-      required={props.required}
-      type={props.type}
-      value={props.value}
-      onChange={onChange}
-      readOnly={props.readOnly}
-    />
-  )
+    if (maxLength && event.target.value.length > maxLength) {
+      return
+    }
+
+    fieldOnChange(event.target.value, { event })
+  }
+
+  render () {
+    const {
+      autoComplete,
+      id,
+      name,
+      required,
+      readOnly,
+      size,
+      type,
+      value
+    } = this.props
+
+    return (
+      <Textarea
+        aria-describedby={this.props['aria-describedby']}
+        autoComplete={autoComplete}
+        className={`textarea is-${size}`}
+        id={id}
+        name={name}
+        required={required}
+        type={type}
+        value={value}
+        onChange={this.onChange}
+        readOnly={readOnly}
+      />
+    )
+  }
 }
 
 export default TextareaInput
