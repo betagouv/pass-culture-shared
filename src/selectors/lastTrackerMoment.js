925a2d995d50dd0343bdb791a052bf65f3c42e90
erwanledoux <lawanledoux@gmail.com>
2018-09-05 23:08:00 +0200

    fixed tracker needed state

---
+++ src/selectors/lastTrackerMoment.js
@@ -0,0 +1,13 @@
+import createCachedSelector from 're-reselect'
+import moment from 'moment'
+
+export default createCachedSelector(
+  state => state.tracker,
+  (tracker, collectionName) => {
+    const trackerKeys = Object.keys(tracker).filter(key =>
+      key.startsWith(collectionName))
+    const lastTrackerMoment = Math.max(...trackerKeys.map(key =>
+      moment(tracker[key])))
+    return lastTrackerMoment
+  }
+)((state, collectionName) => collectionName || '')
