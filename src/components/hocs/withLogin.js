437aa4714cfedd4f9d6d36b108b25a14a1115fa8
erwanledoux <lawanledoux@gmail.com>
2018-09-17 16:15:26 +0200

    pure-component-with-login

--- src/components/hocs/withLogin.js
+++ src/components/hocs/withLogin.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { compose } from 'redux'
@@ -8,23 +8,23 @@ import { requestData } from '../../reducers/data'
 const withLogin = (config = {}) => WrappedComponent => {
   const { failRedirect, successRedirect } = config
 
-  class _withLogin extends Component {
+  class _withLogin extends PureComponent {
 
     componentDidMount = prevProps => {
       const { dispatch, history, location, user } = this.props
 
-      if (user === null) {
-        dispatch(requestData('GET', `users/current`, {
-          handleSuccess: () => {
-            if (successRedirect && successRedirect !== location.pathname)
-              history.push(successRedirect)
-          },
-          handleFail: () => {
-            if (failRedirect && failRedirect !== location.pathname)
-              history.push(failRedirect)
-          },
-        }))
-      }
+      if (user !== null) return
+
+      dispatch(requestData('GET', `users/current`, {
+        handleSuccess: () => {
+          if (successRedirect && successRedirect !== location.pathname)
+            history.push(successRedirect)
+        },
+        handleFail: () => {
+          if (failRedirect && failRedirect !== location.pathname)
+            history.push(failRedirect)
+        },
+      }))
     }
 
     render() {
