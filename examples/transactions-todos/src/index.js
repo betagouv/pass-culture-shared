7283eb3b7b162d83dc7015ec32e6939385cb9984
erwanledoux <lawanledoux@gmail.com>
2018-10-19 14:23:11 +0200

    added text and negates styles

--- examples/transactions-todos/src/index.js
+++ examples/transactions-todos/src/index.js
@@ -5,8 +5,6 @@ import ReactDOM from 'react-dom'
 import './utils/init'
 import Root from './Root'
 
-console.log('registerCacheWorker', registerCacheWorker)
-
 const initApp = () => {
   ReactDOM.render(<Root />, document.getElementById('root'))
   registerCacheWorker()
